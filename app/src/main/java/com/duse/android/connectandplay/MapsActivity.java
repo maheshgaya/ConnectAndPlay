012dc8a13f0f7fdad8cf79105a37cb8f50b679e6
kristinaneel <kristina.neel@drake.edu>
2016-10-16 22:18:25 -0500

    Map
    Tried implementing google map

---
+++ app/src/main/java/com/duse/android/connectandplay/MapsActivity.java
@@ -0,0 +1,27 @@
+package com.duse.android.connectandplay;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by kristinaneel on 10/16/2016.
+ */
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+       MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
+    }
+}
+
