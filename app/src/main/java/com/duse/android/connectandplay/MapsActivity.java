1d8e994a6ff27fcda1d8b4815d1fc13c97b15f44
kristinaneel <kristina.neel@drake.edu>
2016-10-17 21:56:03 -0500

    Functioning Map
    Got the map to function and put a marker on it

--- app/src/main/java/com/duse/android/connectandplay/MapsActivity.java
+++ app/src/main/java/com/duse/android/connectandplay/MapsActivity.java
@@ -3,10 +3,18 @@ package com.duse.android.connectandplay;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
 /**
  * Created by kristinaneel on 10/16/2016.
  */
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
+{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
