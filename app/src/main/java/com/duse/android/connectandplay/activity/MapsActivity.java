d1729dee7aa3430bb846f267006e1bb9ad029557
Mahesh Gaya <mmaheshgaya@gmail.com>
2016-10-20 11:33:27 -0500

    Feat: Added BottomSheet layout
    - Also changed MapActivity to extend AppCompatActivity

--- app/src/main/java/com/duse/android/connectandplay/activity/MapsActivity.java
+++ app/src/main/java/com/duse/android/connectandplay/activity/MapsActivity.java
@@ -2,6 +2,8 @@ package com.duse.android.connectandplay.activity;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.duse.android.connectandplay.R;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,13 +16,16 @@ import com.google.android.gms.maps.model.MarkerOptions;
 /**
  * Created by kristinaneel on 10/16/2016.
  */
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback
 {
 
+    private Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps_bottomsheet);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
