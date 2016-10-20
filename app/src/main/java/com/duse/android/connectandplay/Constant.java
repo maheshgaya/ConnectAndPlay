5256f50818805ee0b014fcb079cc39ab1a570c9e
Mahesh Gaya <mmaheshgaya@gmail.com>
2016-10-20 14:16:46 -0500

    Feat: Added BaseColumns for DB

--- app/src/main/java/com/duse/android/connectandplay/Constant.java
+++ app/src/main/java/com/duse/android/connectandplay/Constant.java
@@ -23,7 +23,7 @@ public class Constant {
     public static final String USER_FIRST_NAME = "First Name";
     public static final String USER_LAST_NAME = "Last Name";
     public static final String USER_BIOGRAPHY = "Biography";
-    
+
     //Sports
     public static final String SPORT_ARRAY = "sports";
     public static final String SPORT_NAME = "name";
--- app/src/main/java/com/duse/android/connectandplay/data/GamesDbHelper.java
+++ app/src/main/java/com/duse/android/connectandplay/data/GamesDbHelper.java
@@ -1,8 +1,27 @@
 package com.duse.android.connectandplay.data;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Mahesh Gaya on 10/20/16.
  */
 
-public class GamesDbHelper {
+public class GamesDbHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "games.db";
+    public static final int DATABASE_VERSION = 1;
+
+    public GamesDbHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
