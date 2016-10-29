1bce793d0d6ea3bed69d688627d9bfba91235bd7
Mahesh Gaya <mmaheshgaya@gmail.com>
2016-10-29 18:09:54 -0500

    Chore: removed unnecessary code

--- app/src/main/java/com/duse/android/connectandplay/fragment/ProfileFragment.java
+++ app/src/main/java/com/duse/android/connectandplay/fragment/ProfileFragment.java
@@ -209,113 +209,12 @@ public class ProfileFragment extends Fragment{
             @Override
             public void onClick(View arg0) {
                 Toast.makeText(getContext(), "Image upload to be implemented in version 2.0.0", Toast.LENGTH_SHORT).show();
-//               selectImage();
 
             }
         });
         return rootView;
     }
 
-//    TODO: This code does not work. Review this if have time
-//    private void selectImage() {
-//
-//        final CharSequence[] items = { "Take Photo", "Choose from Library",
-//                "Cancel" };
-//        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-//        builder.setTitle("Add Photo!");
-//        builder.setItems(items, new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int item) {
-//                boolean result= Utility.checkPermission(getContext());
-//                if (items[item].equals("Take Photo")) {
-//                    userChoosenTask = "Take Photo";
-//                    if(result) {
-//                        cameraIntent();
-//                    }
-//                } else if (items[item].equals("Choose from Library")) {
-//                    userChoosenTask = "Choose from Library";
-//                    if(result) {
-//                        galleryIntent();
-//                    }
-//                } else if (items[item].equals("Cancel")) {
-//                    dialog.dismiss();
-//                }
-//            }
-//        });
-//        builder.show();
-//    }
-//
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-//        switch (requestCode) {
-//            case Utility.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
-//                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                    if(userChoosenTask.equals("Take Photo"))
-//                        cameraIntent();
-//                    else if(userChoosenTask.equals("Choose from Library"))
-//                        galleryIntent();
-//                } else {
-//                    //code for deny
-//                }
-//                break;
-//        }
-//    }
-//    private void galleryIntent()
-//    {
-//        Intent intent = new Intent();
-//        intent.setType("image/*");
-//        intent.setAction(Intent.ACTION_GET_CONTENT);
-//
-//        startActivityForResult(Intent.createChooser(intent, "Select File"),SELECT_FILE);
-//    }
-//    private void cameraIntent()
-//    {
-//        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//        startActivityForResult(intent, REQUEST_CAMERA);
-//    }
-//
-//    //Handling Image
-//    @Override
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//        if (resultCode == Activity.RESULT_OK) {
-//            if (requestCode == SELECT_FILE)
-//                onSelectFromGalleryResult(data);
-//            else if (requestCode == REQUEST_CAMERA)
-//                onCaptureImageResult(data);
-//        }
-//    }
-//    @SuppressWarnings("deprecation")
-//    private void onSelectFromGalleryResult(Intent data) {
-//        Bitmap bm=null;
-//        if (data != null) {
-//            try {
-//                bm = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), data.getData());
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        imageButton.setImageBitmap(bm);
-//    }
-//    private void onCaptureImageResult(Intent data) {
-//        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
-//        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-//        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
-//        File destination = new File(Environment.getExternalStorageDirectory(),
-//                System.currentTimeMillis() + ".jpg");
-//        FileOutputStream fo;
-//        try {
-//            destination.createNewFile();
-//            fo = new FileOutputStream(destination);
-//            fo.write(bytes.toByteArray());
-//            fo.close();
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        imageButton.setImageBitmap(thumbnail);
-//    }
 
     /**
      * Validating form
